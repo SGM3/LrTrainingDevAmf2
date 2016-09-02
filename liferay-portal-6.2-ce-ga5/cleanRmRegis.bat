77580d1bde423493bb2b992c805506a40dcacec9
Shanon Mathai <shanon.mathai@liferay.com>
2016-09-02 10:02:29 -0500

    easliy clean my work for clean reset

--- liferay-portal-6.2-ce-ga5/cleanRmRegis.bat
+++ liferay-portal-6.2-ce-ga5/cleanRmRegis.bat
@@ -1,3 +1,4 @@
 rmdir /s /q tomcat-7.0.62\webapps\custom-signup-portlet;
 rmdir /s /q tomcat-7.0.62\webapps\registraition-portlet;
+rmdir /s /q tomcat-7.0.62\webapps\registration-portlet;
 cleanPortal.bat;
